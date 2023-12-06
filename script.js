a85a4a7bf2ac196e61278f1ee53776d4606438e2
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-12-06 18:29:06 +0900

    Feat: 2023-12-06 작업 PR (#1)
    * Feat: 룰렛 제작 완료
    - 배팅 비율인 +, - 기능 구현
    - 스핀 속도 조절 수정
    - 처음 추가할 떄의 룰렛 색깔 유지하도록 구현
    - 다시하기 버튼 추가
    
    * Fix: 다시하기 버튼 수정

--- script.js
+++ script.js
@@ -2,43 +2,91 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("roulette");
   const ctx = canvas.getContext("2d");
   const spinButton = document.getElementById("spin");
-  const participantInput = document.getElementById("participant-name");
-  const participantWeightInput = document.getElementById("participant-weight");
-  const addParticipantButton = document.getElementById("add-participant");
+  // const participantInput = document.getElementById("participant-name");
+  // const participantWeightInput = document.getElementById("participant-weight");
+  // const addParticipantButton = document.getElementById("add-participant");
+  const resetButton = document.querySelector("#reset");
 
+  let roulettes = [];
   let participants = [];
   let colors = [];
   let angle = 0;
   let isSpinning = false;
   let animationFrameId;
 
+  const tech_7_member = [
+    "공영균",
+    "김일진",
+    "김인경",
+    "김상훈",
+    "김종식",
+    "김동환",
+    "우창욱",
+  ];
+
   const addParticipant = (name, weight) => {
     if (
       name.trim() === "" ||
       weight <= 0 ||
       participants.some((p) => p.name === name)
-    )
+    ) {
       return;
-    participants.push({ name, weight });
-    colors.push("#" + Math.floor(Math.random() * 16777215).toString(16)); // Random color
-    drawRoulette();
+    }
+
+    const existingParticipant = participants.find((p) => p.name === name);
+
+    if (existingParticipant) {
+      // If the participant already exists, use their existing color
+      colors.push(existingParticipant.color);
+    } else {
+      // If it's a new participant, assign a random color
+      const randomColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+      colors.push(randomColor);
+    }
+
+    participants.push({ name, weight, color: colors[colors.length - 1] });
+
+    drawRoulette(name);
   };
 
-  addParticipantButton.addEventListener("click", () => {
-    addParticipant(
-      participantInput.value,
-      parseFloat(participantWeightInput.value)
-    );
-    participantInput.value = "";
-    participantWeightInput.value = "";
-  });
+  // addParticipantButton.addEventListener("click", () => {
+  //   addParticipant(
+  //     participantInput.value,
+  //     parseFloat(participantWeightInput.value)
+  //   );
+  //   participantInput.value = "";
+  //   participantWeightInput.value = "";
+  // });
 
-  const drawRoulette = () => {
+  removeParticipant = (name) => {
+    const index = participants.findIndex((p) => p.name === name);
+
+    if (index !== -1) {
+      const removedParticipant = participants.splice(index, 1)[0];
+      const removedColor = removedParticipant.color;
+
+      const res = roulettes.find((roulette) => roulette.name === name);
+      if (res) {
+        const ctxIndex = roulettes.indexOf(res);
+        roulettes.splice(ctxIndex, 1);
+        res.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Redraw the roulette without the removed participant
+        drawRoulette();
+      }
+
+      // Reuse the removed color for future participants
+      colors.push(removedColor);
+    }
+  };
+
+  const drawRoulette = (name) => {
     if (participants.length === 0) return;
 
     const totalWeight = participants.reduce((acc, p) => acc + p.weight, 0);
     let startAngle = 0;
-
+    const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     participants.forEach((participant, index) => {
@@ -71,6 +119,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
       startAngle = endAngle;
     });
+    roulettes.push({ ctx, name });
+
+    return ctx;
   };
 
   const updateRoulette = () => {
@@ -82,7 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.restore();
   };
 
-  let spinSpeed = 50; // 회전 속도 변수
+  let spinSpeed = 1000; // 회전 속도 변수
   let isStopping = false; // 스핀 멈추기 시작했는지 표시하는 변수
 
   const spinRoulette = () => {
@@ -91,14 +142,17 @@ document.addEventListener("DOMContentLoaded", () => {
       isStopping = false;
       isSpinning = false; // 스핀을 다시 시작할 수 있도록 상태 변경
       spinButton.textContent = "Spin";
-      return;
     }
 
     angle += spinSpeed; // 현재 속도로 회전
 
     // 멈추기 시작했을 때만 속도 감소
     if (isStopping) {
-      spinSpeed *= 0.99; // 스핀 속도 감소
+      document.querySelector("#spin").disabled = "true";
+      // document.querySelector("#spin").style.display = "none";
+      spinSpeed *= Math.random() * (0.995 - 0.99) + 0.99; // 스핀 속도 감소
+      console.log(spinSpeed);
+      resetButton.style.display = "flex";
     }
 
     updateRoulette();
@@ -107,36 +161,86 @@ document.addEventListener("DOMContentLoaded", () => {
 
   spinButton.addEventListener("click", () => {
     if (!isSpinning) {
-      spinSpeed = 0.1; // 초기 스핀 속도 설정
+      spinSpeed = 0.5; // 초기 스핀 속도 설정
       isSpinning = true;
       spinButton.textContent = "Stop";
       requestAnimationFrame(spinRoulette);
     } else {
       isStopping = true; // 스핀 멈추기 시작
+      isSpinning = false;
       spinButton.textContent = "Spin";
     }
   });
   // 엔터키 이벤트 추가
-  participantInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      addParticipant(
-        participantInput.value,
-        parseFloat(participantWeightInput.value)
-      );
-      participantInput.value = "";
-      participantWeightInput.value = "";
-    }
+  // participantInput.addEventListener("keypress", (e) => {
+  //   if (e.key === "Enter") {
+  //     addParticipant(
+  //       participantInput.value,
+  //       parseFloat(participantWeightInput.value)
+  //     );
+  //     participantInput.value = "";
+  //     participantWeightInput.value = "";
+  //   }
+  // });
+
+  // participantWeightInput.addEventListener("keypress", (e) => {
+  //   if (e.key === "Enter") {
+  //     addParticipant(
+  //       participantInput.value,
+  //       parseFloat(participantWeightInput.value)
+  //     );
+  //     participantInput.value = "";
+  //     participantWeightInput.value = "";
+  //   }
+  // });
+  drawRoulette();
+
+  document.querySelectorAll(".plus").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const label = e.target.closest("label");
+      const inputElement = label.previousElementSibling;
+      const ratioElement = label.querySelector(".ratio");
+
+      let currentRatio = parseInt(ratioElement.innerText, 10);
+      currentRatio = Math.min(currentRatio + 1, 10); // 최소값은 1으로 설정
+      ratioElement.innerText = currentRatio.toString();
+
+      removeParticipant(label.htmlFor);
+      inputElement.checked = false;
+    });
   });
 
-  participantWeightInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      addParticipant(
-        participantInput.value,
-        parseFloat(participantWeightInput.value)
-      );
-      participantInput.value = "";
-      participantWeightInput.value = "";
-    }
+  document.querySelectorAll(".minus").forEach((element) => {
+    element.addEventListener("click", (e) => {
+      const label = e.target.closest("label");
+      const inputElement = label.previousElementSibling;
+
+      const ratioElement = label.querySelector(".ratio");
+
+      let currentRatio = parseInt(ratioElement.innerText, 10);
+      currentRatio = Math.max(currentRatio - 1, 1); // 최소값은 1으로 설정
+      ratioElement.innerText = currentRatio.toString();
+
+      removeParticipant(label.htmlFor);
+      inputElement.checked = false;
+    });
   });
-  drawRoulette();
+
+  tech_7_member.forEach((member) => {
+    const memberDOM = document.querySelector(`#${member}`);
+
+    memberDOM.addEventListener("click", (e) => {
+      const memberRatioDOM = document.querySelector(`.${member}.ratio`);
+
+      if (e.target.checked) {
+        addParticipant(e.target.id, parseFloat(memberRatioDOM.innerText));
+      } else {
+        removeParticipant(e.target.id);
+      }
+    });
+  });
+
+  document.querySelector("#reset").addEventListener("click", () => {
+    // location.reload();
+  });
 });
