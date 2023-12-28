af7f9aa4735fbaeceb6d0ad8aade3dc5edbd2387
Jong Sik <otw1917@naver.com>
2023-12-28 20:50:43 +0900

    Refactor: 히스토리가 하나여도 가중치 변경되게함

--- src/index.js
+++ src/index.js
@@ -37,7 +37,18 @@ app.get("/user/list", async (req, res) => {
     // 최근 기록 3개를 가져옵니다.
     const latestHistory = await History.find().sort({ createdAt: -1 }).limit(3);
 
+    if (latestHistory.length == 1) {
+      // 한 건만 존재하면, 해당 유저의 가중치를 0.7로 설정합니다.
+      const users = await User.find();
+      users.forEach((user) => {
+        if (user._id.toString() === latestHistory[0].user.toString()) {
+          user.weight *= 0.7;
+        }
+      });
+    }
+
     // latestHistory가 존재하지 않으면, 유저 목록을 랜덤으로 섞어 반환합니다.
+
     if (latestHistory.length < 2) {
       const users = await User.find();
       users.sort(() => Math.random() - 0.5);
