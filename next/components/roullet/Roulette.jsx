6480eacf8b015edf894de8b1cb482e16a2dd14b6
wukdddang <dnr8874@naver.com>
2024-01-04 17:56:08 +0900

    Feat: Roulette 컴포넌트 초안 추가

---
+++ next/components/roullet/Roulette.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import FetchClass from "@/app/service/fetch";
+import { useState } from "react";
+
+const Roulette = () => {
+  const [data, setData] = useState(null);
+  const fetchInstance = new FetchClass();
+
+  const handleFetch = async () => {
+    try {
+      const url = "http://localhost:3000/api/user/list";
+      const options = { method: "GET" }; // 요청 메서드와 필요한 옵션
+      const responseData = await fetchInstance.runFetch(url, options);
+      setData(responseData); // 응답 데이터 상태에 저장
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleFetch}>Fetch Data</button>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
+};
+
+export default Roulette;
